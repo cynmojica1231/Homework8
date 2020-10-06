5df5416b3135cec9df97ae12b8f9ed225d3cfb27
Cynthia Mojica <cmojica1231@gmail.com>
2020-10-05 19:37:33 -0700

    modify gitignore, index.js, pacjage.json

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-const fs = require("fs");
-const path = require("path");
 const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+// Internal modules
 const generateMarkdown = require("./utils/generateMarkdown");
 
 const questions = [
   {
     type: "input",
     name: "github",
-    message: "What is your GitHub username?"
-  },{
+    message: "What is your GitHub username?",
+  },
+  {
     type: "input",
     name: "email",
-    message: "What is your email address?"
+    message: "What is your email address?",
   },
   {
     type: "input",
     name: "title",
-    message: "What is your project's name?"
+    message: "What is your project's name?",
   },
   {
     type: "input",
     name: "description",
-    message: "Please write a short description of your project"
+    message: "Please write a short description of your project",
   },
   {
     type: "list",
     name: "license",
-    message: "What kind of license should your project have?",
-    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    message: "Choose a license for your project.",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "Apache License 2.0", "None"],
   },
   {
     type: "input",
     name: "installation",
     message: "What command should be run to install dependencies?",
-    default: "npm i"
+    default: "npm i",
   },
   {
     type: "input",
     name: "test",
     message: "What command should be run to run tests?",
-    default: "npm test"
+    default: "npm test",
   },
   {
     type: "input",
@@ -49,20 +52,26 @@ const questions = [
   {
     type: "input",
     name: "contributing",
-    message: "What does the user need to know about contributing to the repo?",
-  }
+    message:
+      "If applicable, provide guidelines on how other developers can contribute to your project.",
+  },
 ];
 
 function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success! Your README.md file has been generated");
+  });
 }
 
 function init() {
-  inquirer.prompt(questions)
-  .then((inquirerResponses) => {
+  inquirer.prompt(questions).then((inquirerResponses) => {
     console.log("Generating README...");
-    writeToFile("README.md", generateMarkdown({...inquirerResponses}));
-  })
+    writeToFile("README.md", generateMarkdown({ ...inquirerResponses }));
+  });
 }
 
 init();
